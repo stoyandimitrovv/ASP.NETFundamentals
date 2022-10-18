85856fc1c925bab40ce66be46cdca3d8c01e73c8
Stoyan <stoqnd1999@gmail.com>
2022-10-18 23:28:29 +0300

    TaskApp

---
+++ TaskBoardApp/TaskBoardApp/Data/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using static TaskBoardApp.Data.DataConstants.User;
+
+namespace TaskBoardApp.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(MaxUserFirstName)]
+        public string FirstName { get; init; }
+
+        [Required]
+        [MaxLength(MaxUserLastName)]
+        public string LastName { get; init; }
+
+    }
+}
---
+++ TaskBoardApp/TaskBoardApp/Data/Migrations/20221018150309_TaskApp.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskBoardApp.Data.Migrations
+{
+    public partial class TaskApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BoardId = table.Column<int>(type: "int", nullable: false),
+                    OwnerId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "35994363-e1fd-4ebf-b366-27b30e9af02f", 0, "64f5dde8-3e8d-4660-a08e-b1924024a022", "guest@mail.com", false, "Guest", "User", false, null, "GUEST@MAIL.COM", "GUEST", "AQAAAAEAACcQAAAAEPIX+7AJiu1o+niK4+sKSgvTtgSgeFssyYBE5FR525s/bhDJJQ1kx2MpjB3HsNO7gQ==", null, false, "5a8dba70-2219-49f1-a451-f6eb87b5302a", false, "guest" });
+
+            migrationBuilder.InsertData(
+                table: "Boards",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Open" },
+                    { 2, "In Progress" },
+                    { 3, "Done" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "BoardId", "CreatedOn", "Description", "OwnerId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2022, 9, 18, 18, 3, 8, 838, DateTimeKind.Local).AddTicks(9907), "Learn using EF and MS Studio", "35994363-e1fd-4ebf-b366-27b30e9af02f", "Prepare for ASP.Net Fundamentals Exam" },
+                    { 2, 3, new DateTime(2022, 5, 18, 18, 3, 8, 838, DateTimeKind.Local).AddTicks(9957), "Learn using ASP.Net Core Identity", "35994363-e1fd-4ebf-b366-27b30e9af02f", "Improve EF Core skills" },
+                    { 3, 2, new DateTime(2021, 12, 18, 18, 3, 8, 838, DateTimeKind.Local).AddTicks(9961), "Learn using ASP.Net Core Identity", "35994363-e1fd-4ebf-b366-27b30e9af02f", "Improve ASP.Net Core skills" },
+                    { 4, 3, new DateTime(2022, 9, 18, 18, 3, 8, 838, DateTimeKind.Local).AddTicks(9965), "Prepare for solving old Mid and Final exams", "35994363-e1fd-4ebf-b366-27b30e9af02f", "Prepare for C# Fundamentals Exam" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_BoardId",
+                table: "Tasks",
+                column: "BoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_OwnerId",
+                table: "Tasks",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "35994363-e1fd-4ebf-b366-27b30e9af02f");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
