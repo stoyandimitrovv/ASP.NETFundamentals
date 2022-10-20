1c7dc7ec8beeadb467cef7e5e23670452985f2e1
Stoyan <stoqnd1999@gmail.com>
2022-10-20 23:46:38 +0300

    Board page

--- TaskBoardApp/TaskBoardApp/Controllers/BoardsController.cs
+++ TaskBoardApp/TaskBoardApp/Controllers/BoardsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TaskBoardApp.Data;
+using TaskBoardApp.Models;
+using TaskBoardApp.Models.Task;
 
 namespace TaskBoardApp.Controllers
 {
@@ -11,9 +13,22 @@ namespace TaskBoardApp.Controllers
 
         public IActionResult All()
         {
+            var boards = this.data.Boards
+                .Select(b => new BoardViewModel()
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                    Tasks = b.Tasks.Select(t => new TaskViewModel()
+                    {
+                        Id = t.Id,
+                        Title = t.Title,
+                        Description = t.Description,
+                        Owner = t.Owner.UserName
+                    })
+                })
+                .ToList();
 
-
-            return View();
+            return View(boards);
         }
     }
 }
