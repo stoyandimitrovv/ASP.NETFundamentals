bee3b4770e6d4c5610ae04a8585b7f0ed6244dfd
Stoyan <stoqnd1999@gmail.com>
2022-10-22 00:31:35 +0300

    Prep

---
+++ ExamPrep/Watchlist/Contracts/IMovieService.cs
@@ -0,0 +1,20 @@
+﻿using Watchlist.Data.Entities;
+using Watchlist.Models;
+
+namespace Watchlist.Contracts
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<MovieViewModel>> GetAllAsync();
+
+        Task<IEnumerable<MovieViewModel>> GetWatchedAsync(string userId);
+
+        Task<IEnumerable<Genre>> GetGenresAsync();
+
+        Task AddMovieAsync(AddMovieViewModel model);
+
+        Task AddMovieToCollectionAsync(int movieId, string userId);
+
+        Task RemoveMovieFromCollectionAsync(int movieId, string userId);
+    }
+}
---
+++ ExamPrep/Watchlist/Data/Entities/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Watchlist.Data.Entities
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Director { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(GenreId))]
+        public Genre Genre { get; set; }
+
+        public List<UserMovie> UsersMovies { get; set; }
+    }
+}
