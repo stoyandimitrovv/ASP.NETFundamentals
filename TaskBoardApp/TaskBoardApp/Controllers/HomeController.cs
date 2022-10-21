e1f1584b666a41a369df7b5674f4bac0a8541b35
Stoyan <stoqnd1999@gmail.com>
2022-10-21 18:12:25 +0300

    Final App

--- TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
+++ TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -1,27 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+using TaskBoardApp.Data;
 using TaskBoardApp.Models;
 
 namespace TaskBoardApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly TaskBoardAppDbContext data;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(TaskBoardAppDbContext context)
+            => this.data = context;
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
-            return View();
+            var taskBoards = this.data
+                .Boards
+                .Select(b => b.Name)
+                .Distinct();
+
+            var tasksCount = new List<HomeBoardModel>();
+            foreach (var boardName in taskBoards)
+            {
+                var tasksInBoard = this.data.Tasks.Where(t => t.Board.Name == boardName).Count();
+
+                tasksCount.Add(new HomeBoardModel()
+                {
+                    BoardName = boardName,
+                    TasksCount = tasksInBoard
+                });
+            }
+
+            var userTasksCount = -1;
+
+            if (this.User.Identity.IsAuthenticated)
+            {
+                var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                userTasksCount = this.data.Tasks.Where(t => t.OwnerId == currentUserId).Count();
+            }
+
+            var homeModel = new HomeViewModel()
+            {
+                AllTasksCount = this.data.Tasks.Count(),
+                BoardsWithTasksCount = tasksCount,
+                UserTasksCount = userTasksCount
+            };
+
+            return View(homeModel);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
---
+++ TaskBoardApp/TaskBoardApp/Controllers/TaskController.cs
@@ -0,0 +1,184 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using TaskBoardApp.Data;
+using TaskBoardApp.Models.Task;
+using Task = TaskBoardApp.Data.Entities.Task;
+
+namespace TaskBoardApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly TaskBoardAppDbContext data;
+
+        public TaskController(TaskBoardAppDbContext context)
+            => this.data = context;
+
+        public IActionResult Create()
+        {
+            TaskFormModel taskModel = new TaskFormModel()
+            {
+                Boards = GetBoards()
+            };
+
+            return View(taskModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskFormModel taskModel)
+        {
+            if (!GetBoards().Any(b => b.Id == taskModel.BoardId))
+            {
+                this.ModelState.AddModelError(nameof(taskModel.BoardId), "Board does not exist");
+            }
+
+            string currentUserId = GetUserId();
+            Task task = new Task()
+            {
+                Title = taskModel.Title,
+                Description = taskModel.Description,
+                CreatedOn = DateTime.Now,
+                BoardId = taskModel.BoardId,
+                OwnerId = currentUserId
+            };
+            this.data.Tasks.Add(task);
+            this.data.SaveChanges();
+
+            var boards = this.data.Boards;
+
+            return RedirectToAction("All", "Boards");
+        }
+
+        private IEnumerable<TaskBoardModel> GetBoards()
+            => this.data.Boards.Select(x => new TaskBoardModel()
+            {
+                Id = x.Id,
+                Name = x.Name
+            });
+
+        private string GetUserId()
+            => this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public IActionResult Details(int id)
+        {
+            var task = this.data
+                .Tasks
+                .Where(t => t.Id == id)
+                .Select(t => new TaskDetailsViewModel()
+                {
+                    Id = t.Id,
+                    Title = t.Title,
+                    Description = t.Description,
+                    CreatedOn = t.CreatedOn.ToString("dd/MM/yyyy HH:mm"),
+                    Board = t.Board.Name,
+                    Owner = t.Owner.UserName
+                })
+                .FirstOrDefault();
+
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            return View(task);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Task task = data.Tasks.Find(id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            TaskFormModel taskModel = new TaskFormModel()
+            {
+                Title = task.Title,
+                Description = task.Description,
+                BoardId = task.BoardId,
+                Boards = GetBoards()
+            };
+
+            return View(taskModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, TaskFormModel taskModel)
+        {
+            Task task = data.Tasks.Find(id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            if (!GetBoards().Any(b => b.Id == taskModel.BoardId))
+            {
+                this.ModelState.AddModelError(nameof(taskModel.BoardId), "Board does not exist.");
+            }
+
+            task.Title = taskModel.Title;
+            task.Description = taskModel.Description;
+            task.BoardId = taskModel.BoardId;
+
+            this.data.SaveChanges();
+            return RedirectToAction("All", "Boards");
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            Task task = data.Tasks.Find(id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            TaskViewModel taskModel = new TaskViewModel()
+            {
+                Id = task.Id, 
+                Title = task.Title,
+                Description = task.Description,
+            };
+
+            return View(taskModel);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(TaskViewModel taskModel)
+        {
+            Task task = data.Tasks.Find(taskModel.Id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            this.data.Tasks.Remove(task);
+            this.data.SaveChanges();
+
+            return RedirectToAction("All", "Boards");
+        }
+    }
+}
